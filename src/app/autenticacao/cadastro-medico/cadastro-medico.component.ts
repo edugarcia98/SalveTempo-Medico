9e1b0f6d455456a029a70dc5d8514a7cfd6fa3ef
edugarcia98 <edugarcia758@gmail.com>
2020-11-02 14:39:32 -0300

    Tratativa de erros na tela de cadastro

--- src/app/autenticacao/cadastro-medico/cadastro-medico.component.ts
+++ src/app/autenticacao/cadastro-medico/cadastro-medico.component.ts
@@ -18,6 +18,7 @@ import { Router } from '@angular/router';
 export class CadastroMedicoComponent implements OnInit {
 
   especializacoes: Especializacao[];
+  error_found: boolean;
   error: any;
   selectedSexo = '';
   selectedEspecializacao = 0;
@@ -27,7 +28,6 @@ export class CadastroMedicoComponent implements OnInit {
     private especializacaoService: EspecializacaoService,
     // private menuVisibilityService: MenuVisibilityService,
     private router: Router) {
-
   }
 
   ngOnInit() {
@@ -43,6 +43,15 @@ export class CadastroMedicoComponent implements OnInit {
   }
 
   addMedico(medicoNome: string, medicoDataNasc: Date, medicoCRM: string, medicoEmail: string, medicoSenha: string, medicoSenhaConfirm: string) {
+    this.error_found = false;
+    this.error = {
+      "username": {"desc": "Nome", "content": []},
+      "email": {"desc": "E-mail", "content": []},
+      "password1": {"desc": "Senha", "content": []},
+      "password2": {"desc": "Confirmação de senha", "content": []},
+      "non_field_errors": {"desc": "Outros", "content": []}
+    };
+
     this.cadastroMedicoService.cadastroUsuario(medicoEmail, medicoNome, medicoSenha, medicoSenhaConfirm).subscribe(
       () => {
         this.cadastroMedicoService.getUsuarioByEmail(medicoEmail).subscribe(
@@ -60,18 +69,26 @@ export class CadastroMedicoComponent implements OnInit {
               );
           },
           (error: any) => {
-            this.error = error;
-            console.log(this.error);
+            this.error_found = true;
+            this.error["non_field_errors"]["content"].push("Usuário não encontrado");
           }
         );
       },
       (error: any) => {
-        this.error = error;
-        console.log(this.error);
+        this.error_found = true;
+        this.errorHandler(error);
       }
     );
+  }
 
-    //location.reload();
+  errorHandler(error: any) {
+    for (let key in this.error) {
+      if (error.error[key]) {
+        for (let err of error.error[key]) {
+          this.error[key]["content"].push(err);
+        }
+      }
+    }
   }
 
 }
